44dc69ac266eb2433e47bafef325f6408acc1afc
Inertia-OnlineClass <courseinertia@gmail.com>
2021-06-30 21:03:02 +0630

    Upload Project

---
+++ code/.loop.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+clear
+echo -en "\e[0mEnter \e[33mText\e[0m You Want to Loop : "
+read txt
+echo -en "Enter looping \e[33mTimes\e[0m : "
+read count
+str=""
+for ((x=0 ; x<=count ; x++)); do
+	str+="$txt \n";
+	done
+termux-clipboard-set $str
---
+++ inertia.sh
@@ -0,0 +1,53 @@
+﻿#!/bin/bash
+clear
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+apt install figlet
+clear
+
+echo -e "$YELLOW"
+figlet "Tool"
+echo -e "$CYAN"
+figlet "Inertia v2"
+
+echo -e "$GREEN"
+
+echo "				written by inertia"
+ppwd="${PWD}/"
+cd code
+mv .cmd.sh $ppwd
+mv .theme.sh $ppwd
+mv .loop.sh $ppwd
+cd ..
+
+cmd(){
+bash .cmd.sh
+}
+theme(){
+bash .theme.sh
+}
+loop(){
+bash .loop.sh
+}
+echo -e "	${YELLOW}[1] Theme Changer."
+echo "	[2] Cmd Maker."
+echo -e "	[3] Text Looper.${NONE}"
+echo -e "\n"
+printf "\e[1;31m Select Option : \e[0m"
+read options
+if [ "$options" -eq "1" ];then
+         theme
+fi
+if [ "$options" -eq "2" ];then
+         cmd
+fi
+if [ "$options" -eq "3" ];then
+         loop
+fi
